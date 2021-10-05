f9b5ffeb789c17bafc7027f65da7706529f3521a
quychi <nguyenquychi96@gmail.com>
2021-10-05 21:20:25 +0700

    add private route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import React, { Suspense, lazy, useState, useEffect } from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
@@ -6,24 +7,56 @@ import Login from './pages/Login';
 import Chat2 from './pages/Chat2';
 import UserInfo from './pages/Membership';
 import ConversationListItem from './common/components/ConversationListItem';
+import { auth } from './services/firebase';
 
 import 'antd/dist/antd.css';
 
 import { connect } from 'react-redux';
 
+import PrivateRoute from './common/components/PrivateRoute';
 function App() {
     return (
+    const [state, setState] = useState({
+        authenticated: false,
+        loading: true
+    });
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setState({
+                    authenticated: true,
+                    loading: false
+                });
+            } else {
+                setState({
+                    authenticated: false,
+                    loading: false
+                });
+            }
+        });
+    }, []);
         <div style={{ fontFamily: 'Avenir' }}>
             <Router>
-                <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route path="/usersInfo" component={UserInfo} />
-                    <Route
-                        path="/conversationListItem"
-                        component={ConversationListItem}
-                    />
-                    <Route path="/chats" component={Chat2} />
-                </Switch>
+                    <Switch>
+                        {/* <PublicRoute path="/signup" authenticated={state.authenticated} component={Signup}></PublicRoute> */}
+                        <Route exact path="/" component={Login} />
+                        <PrivateRoute
+                            path="/usersInfo"
+                            authenticated={state.authenticated}
+                            component={UserInfo}
+                        ></PrivateRoute>
+                        <PrivateRoute
+                            path="/conversationListItem"
+                            authenticated={state.authenticated}
+                            component={ConversationListItem}
+                        ></PrivateRoute>
+                        <PrivateRoute
+                            path="/chats"
+                            authenticated={state.authenticated}
+                            component={Chat2}
+                        ></PrivateRoute>
+                    </Switch>
             </Router>
         </div>
     );
---
+++ src/common/components/PrivateRoute/index.js
@@ -0,0 +1,23 @@
+import { Redirect, Route } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, authenticated, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={(props) =>
+                authenticated === true ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: '/login',
+                            state: { from: props.location }
+                        }}
+                    />
+                )
+            }
+        />
+    );
+};
+
+export default PrivateRoute;
